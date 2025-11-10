74787e520ea447d0385dcdb98ef8eef0481004e4
Romesh Abeyawardena <Romesh.Abeyawardena@bellrock.co.uk>
2025-11-10 13:06:13 +0000

    SharedPathLink

---
+++ geo-auth/Endpoints/Endpoints.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+
+namespace geo_auth;
+
+public static class Endpoints
+{
+    [Function("PasswordSalter")]
+    public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest request,
+        FunctionContext executionContext,
+        CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+        return new JsonResult(true);
+    }
+}
