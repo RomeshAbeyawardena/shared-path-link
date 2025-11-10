56cd6cf857895f621130ef03eff76a4faa849f25
Romesh Abeyawardena <Romesh.Abeyawardena@bellrock.co.uk>
2025-11-10 14:50:50 +0000

    wip

--- geo-auth/Endpoints/Endpoints.cs
+++ geo-auth/Endpoints/Endpoints.cs
@@ -15,12 +15,29 @@ public static class Endpoints
     public static async Task<IResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest request,
         FunctionContext executionContext)
     {
-        if (request.QueryString.HasValue)
+        string[] acceptableEncodings = ["jwt"];
+        try
         {
-            return new StandardErrorResponse(
-                "Sensitive parameters should not be passed in the URL of a public method. If using a JWT token, safely encode it and include the header 'Accept-Encoding: jwt'. Supply the token as a 'token' field in the request body or as form data to ensure secure transmission.", 400);
+            if (request.QueryString.HasValue)
+            {
+                throw new ResponseException("Sensitive parameters should not be passed in the URL of a public method. If using a JWT token, safely encode it and include the header 'Accept-Encoding: jwt'. Supply the token as a 'token' field in the request body or as form data to ensure secure transmission.", StatusCodes.Status400BadRequest);
+            }
+
+            if (!request.Headers.AcceptEncoding.Any(acceptableEncodings.Contains))
+            {
+                throw new ResponseException("An acceptable content type was not specified. Include the header 'Accept-Encoding: jwt' in your request.", StatusCodes.Status422UnprocessableEntity);
+            }
+
+            await Task.CompletedTask;
+            return new PasswordSalterResponse();
         }
-        await Task.CompletedTask;
-        return new PasswordSalterResponse();
+        catch (ResponseException ex)
+        {
+            return new StandardErrorResponse(ex);
+        }
+        catch (Exception ex)
+        {
+            return new StandardErrorResponse(ex, StatusCodes.Status400BadRequest);
+        }
     }
 }
