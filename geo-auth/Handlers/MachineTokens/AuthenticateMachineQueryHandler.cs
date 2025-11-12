c90a87d6e2667e65a92d1fa7ca50c17538639fb4
Romesh Abeyawardena <Romesh.Abeyawardena@bellrock.co.uk>
2025-11-12 11:30:27 +0000

    Updated handler

--- geo-auth/Handlers/MachineTokens/AuthenticateMachineQueryHandler.cs
+++ geo-auth/Handlers/MachineTokens/AuthenticateMachineQueryHandler.cs
@@ -1,26 +1,96 @@
-﻿using Azure.Data.Tables;
-using geo_auth.Models;
+﻿using Azure;
+using Azure.Data.Tables;
 using geo_auth.Extensions;
+using geo_auth.Handlers.Tokens;
+using geo_auth.Models;
+using GeoAuth.Shared.Exceptions;
 using GeoAuth.Shared.Requests.MachineToken;
 using MediatR;
-using Microsoft.Extensions.DependencyInjection;
-using GeoAuth.Shared.Exceptions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace geo_auth.Handlers.MachineTokens;
 
-internal class AuthenticateMachineQueryHandler([FromKeyedServices("machine-token")] TableClient tableClient) : IRequestHandler<AuthenticateMachineQuery, AuthenticateMachineResult>
+internal class AuthenticateMachineQueryHandler([FromKeyedServices("machine-token")] TableClient machineTableClient,
+    [FromKeyedServices("machine-access-token")] TableClient machineAccessTokenTableClient,
+    TimeProvider timeProvider, IOptions<TokenConfiguration> tokenConfigurationOptions)
+    : IRequestHandler<AuthenticateMachineQuery, AuthenticateMachineResult>
 {
+    private string GenerateToken(MachineData request, CancellationToken cancellationToken)
+    {
+        var tokenConfiguration = tokenConfigurationOptions.Value;
+
+        var signingKey = tokenConfiguration.SigningKey ?? throw new ResponseException("Signing key missing", StatusCodes.Status500InternalServerError);
+        var key = new SymmetricSecurityKey(Convert.FromBase64String(signingKey))
+        {
+            KeyId = tokenConfiguration.SigningKeyId ?? throw new ResponseException("Signing key ID missing", StatusCodes.Status500InternalServerError)
+        };
+        var utcNow = timeProvider.GetUtcNow();
+
+        var descriptor = new SecurityTokenDescriptor
+        {
+            Issuer = tokenConfiguration.ValidAudience,
+            Audience = tokenConfiguration.ValidAudience,
+            Claims =
+                {
+                    { "machine-id", request.PartitionKey },
+                    { "row-key", request.RowKey }
+                },
+            Expires = utcNow.UtcDateTime,
+            SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+        };
+
+        var handler = new JsonWebTokenHandler();
+        return handler.CreateToken(descriptor);
+    }
+
     public async Task<AuthenticateMachineResult> Handle(AuthenticateMachineQuery request, CancellationToken cancellationToken)
     {
 
-        var result = await tableClient.QueryAsync<MachineData>($"MachineId eq '{request.MachineId}' AND Secret eq '{request.Secret}'", 1, cancellationToken: cancellationToken).FirstOrDefaultAsync(cancellationToken);
-        
+        var result = await machineTableClient.QueryAsync<MachineData>($"PartitionKey eq '{request.MachineId}' AND Secret eq '{request.Secret}'", 1,
+            cancellationToken: cancellationToken).FirstOrDefaultAsync(cancellationToken);
+
         if (result is null)
         {
             return new AuthenticateMachineResult(null, new ResponseException("Machine authentication failed", StatusCodes.Status401Unauthorized));
         }
 
-        return new AuthenticateMachineResult(new MachineToken(""));
+        var utcNowDate = timeProvider.GetUtcNow().UtcDateTime;
+        var accessToken = await machineAccessTokenTableClient.QueryAsync<MachineDataAccessToken>(
+            $"PartitionKey eq '{request.MachineId}' AND ValidFrom le datetime'{utcNowDate:O}' AND Expires ge datetime'{utcNowDate:O}'", 1, cancellationToken: cancellationToken)
+                .FirstOrDefaultAsync(cancellationToken);
+
+        if (accessToken is not null)
+        {
+            //serves existing token
+            return new AuthenticateMachineResult(new MachineToken(accessToken.Token));
+        }
+
+        var utcNow = timeProvider.GetUtcNow();
+
+        var newToken = GenerateToken(request, cancellationToken);
+        var response = await machineAccessTokenTableClient.AddEntityAsync(new MachineDataAccessToken
+        {
+            PartitionKey = request.MachineId.GetValueOrDefault().ToString(),
+            RowKey = Guid.NewGuid().ToString(),
+            Token = newToken,
+            ValidFrom = utcNow,
+            Expires = utcNow.AddHours(2),
+            Timestamp = utcNow,
+            ETag = ETag.All
+        }, cancellationToken);
+
+        if (!response.IsError)
+        {
+            return new AuthenticateMachineResult(new MachineToken(newToken));
+        }
+
+        return new AuthenticateMachineResult(null, new ResponseException("Unable to generate token",
+            StatusCodes.Status500InternalServerError, new Exception(response.ReasonPhrase)));
     }
 }
--- geo-auth/Models/MachineData.cs
+++ geo-auth/Models/MachineData.cs
@@ -5,7 +5,6 @@ namespace geo_auth.Models;
 
 internal record MachineData : ITableEntity
 {
-    public Guid MachineId { get; set; }
     public string? Secret { get; set; }
     public required string PartitionKey { get; set; }
     public required string RowKey { get; set; }
@@ -15,7 +14,6 @@ internal record MachineData : ITableEntity
 
 internal record MachineDataAccessToken : ITableEntity
 {
-    public Guid MachineId { get; set; }
     public string? Token { get; set; }
     public DateTimeOffset ValidFrom { get; set; }
     public DateTimeOffset Expires { get; set; }
