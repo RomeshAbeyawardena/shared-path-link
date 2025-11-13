fdd8ba7b12d46229c02b7ac27333ddbcdf8c2462
Romesh Abeyawardena <Romesh.Abeyawardena@bellrock.co.uk>
2025-11-13 08:54:47 +0000

    Handle errors

--- geo-auth/Handlers/MachineTokens/UpdateMachineAccessTokenCommandHandler.cs
+++ geo-auth/Handlers/MachineTokens/UpdateMachineAccessTokenCommandHandler.cs
@@ -5,10 +5,11 @@ using geo_auth.Models;
 using GeoAuth.Shared.Requests.MachineToken;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace geo_auth.Handlers.MachineTokens
 {
-    internal class UpdateMachineAccessTokenCommandHandler(
+    internal class UpdateMachineAccessTokenCommandHandler(ILogger<UpdateMachineAccessTokenCommandHandler> logger,
         [FromKeyedServices(KeyedServices.MachineAccessTokenTable)] TableClient machineAccessTokenTableClient,
         TimeProvider timeProvider) 
         : IRequestHandler<UpdateMachineAccessTokenCommand>
@@ -28,7 +29,7 @@ namespace geo_auth.Handlers.MachineTokens
 
             if (response.IsError)
             {
-                
+                logger.LogError("{status}: {reason}", response.Status, response.ReasonPhrase);
             }
         }
     }
