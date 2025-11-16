7603969a24b472fe27dcdef988962c89abaa7b84
Romesh Abeyawardena <romesh.a@outlook.com>
2025-11-16 13:31:47 +0000

    wip

--- GeoAuth.Infrastructure.Azure/Models/DbMachineData.cs
+++ GeoAuth.Infrastructure.Azure/Models/DbMachineData.cs
@@ -6,6 +6,7 @@ namespace GeoAuth.Infrastructure.Azure.Models;
 
 public class DbMachineData : MappableBase<IMachineData>, IMachineData, ITableEntity
 {
+    private static Guid GetGuid(string value) => string.IsNullOrWhiteSpace(value) || !Guid.TryParse(value, out var id) ? Guid.Empty : id;
     protected override IMachineData Source => this;
     public string? Secret { get; set; }
     
@@ -13,8 +14,8 @@ public class DbMachineData : MappableBase<IMachineData>, IMachineData, ITableEnt
     public ETag ETag { get; set; }
     public string PartitionKey { get; set; } = default!;
     public string RowKey { get; set; } = default!;
-    Guid IMachineData.MachineId { get => string.IsNullOrWhiteSpace(PartitionKey) || !Guid.TryParse(PartitionKey, out var machineId) ? Guid.Empty : machineId; }
-    Guid IMachineData.Id { get => string.IsNullOrWhiteSpace(RowKey) || !Guid.TryParse(RowKey, out var id) ? Guid.Empty : id; }
+    Guid IMachineData.MachineId { get => GetGuid(PartitionKey); }
+    Guid IMachineData.Id { get => GetGuid(RowKey); }
 
     public override void Map(IMachineData source)
     {
